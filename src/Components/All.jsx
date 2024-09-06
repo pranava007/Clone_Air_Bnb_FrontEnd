2ff4e61fe5c92a78855c4c270b6909b12ccd6cfd
pranava007 <pranavamuthu000@gmail.com>
2024-09-06 13:44:55 +0530

    check

--- src/Components/All.jsx
+++ src/Components/All.jsx
@@ -6,7 +6,7 @@ const All = () => {
   const { properties } = useSelector((state) => state.properties);
   const { bookingInfo  } = useSelector((state)=>state.bookingInfo)
   console.log( 'Trending', properties);
-  console.log( 'Trending', bookingInfo);
+  console.log( 'bookinginfo', bookingInfo);
   // const {} =  useSelector((state)=>)
 
   return (
