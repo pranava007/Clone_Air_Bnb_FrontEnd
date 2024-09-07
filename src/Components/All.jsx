ba90627d1cc52a4726c2e4b03d4707823983e29e
pranava007 <pranavamuthu000@gmail.com>
2024-09-07 15:00:39 +0530

    add comment

--- src/Components/All.jsx
+++ src/Components/All.jsx
@@ -4,35 +4,28 @@ import Cart from "./Cart";
 
 const All = () => {
   const { properties } = useSelector((state) => state.properties);
-  const { bookingInfo  } = useSelector((state)=>state.bookingInfo);
-  console.log( 'Trending', properties);
-  console.log( 'bookinginfo', bookingInfo);
-  console.log( 'bookinginfo', bookingInfo.bookings.status === "confirmed");
+  const { bookingInfo } = useSelector((state) => state.bookingInfo);
+  // console.log( 'Trending', properties);
+  // console.log( 'bookinginfo', bookingInfo);
+  // console.log( 'bookinginfo', bookingInfo.bookings.status === "confirmed");
 
+  const confirmedBookings = bookingInfo.bookings.filter(
+    (booking) => booking.status === "confirmed"
+  );
 
-  const confirmedBookings = bookingInfo.bookings.filter((booking) => booking.status === "confirmed");
-
-
-  console.log( 'Trending', properties[0]._id);
-  console.log("confirm book",confirmedBookings[0].propertyId._id);
-
-
+  // console.log("Trending", properties[0]._id);
+  // console.log("confirm book", confirmedBookings[0].propertyId._id);
 
   // Assuming confirmedBookings is already filtered for "confirmed" bookings
-const confirmedPropertyIds = confirmedBookings.map((booking) => booking.propertyId._id);
+  const confirmedPropertyIds = confirmedBookings.map(
+    (booking) => booking.propertyId._id
+  );
 
-const filteredProperties = properties.filter(
-  (property) => !confirmedPropertyIds.includes(property._id)
-);
+  const filteredProperties = properties.filter(
+    (property) => !confirmedPropertyIds.includes(property._id)
+  );
 
-console.log('Filtered Properties:', filteredProperties);
-
-  
-
-
- 
-  
-
+  // console.log('Filtered Properties:', filteredProperties);
 
   return (
     <>
